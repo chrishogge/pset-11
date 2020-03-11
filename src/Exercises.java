00d241aa7ee27c2c16bacc928bd9bf5abf52da36
chris.hogge <chogge@ucvts.org>
2020-03-11 14:16:17 -0400

    Exercise 1 and 2 working on basic level

--- src/Exercises.java
+++ src/Exercises.java
@@ -1,12 +1,35 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Exercises {
 
   public int findMe(int[] list, int target) {
+
+    int i = 0;
+
+    for(int x: list){
+      if(x == target){
+        return i;
+      } else{
+        i++;
+      }
+    }
+
     return -1;
   }
 
   public int findMe(ArrayList<String> list, String target) {
+
+    int i = 0;
+
+    for(String tempStr: list){
+      if(tempStr.equals(target)){
+        return i;
+      }
+
+      i++;
+    }
+
     return -1;
   }
 
@@ -48,5 +71,16 @@ public class Exercises {
 
   public String[] merge(String[] list, boolean ascending) {
     return null;
+  }
+
+  public static void main(String args[]){
+
+    Exercises ex = new Exercises();
+
+    int[] test = {1, 2, 3, 5, 7, 8, 9, 9, 10};
+    System.out.println(ex.findMe(test,9));
+
+    ArrayList<String> testStrings = new ArrayList<String>(Arrays.asList("Hello", "Howdy", "Hey", "Sup"," Sup"));
+    System.out.println(ex.findMe(testStrings, "Sup"));
   }
 }
